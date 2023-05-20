3d9c7dcb7009d37de82f5cc403fba813bf8a115e
Szymon Oleszek <szymqwerty@gmail.com>
2023-05-20 14:15:21 +0200

    descriptions

--- src/002_basics/exercise001/ComplexCounterWithRedux.js
+++ src/002_basics/exercise001/ComplexCounterWithRedux.js
@@ -12,6 +12,19 @@ import { Output } from "./Output";
  * żeby obsłużyc counter należy użyć reduxa oraz slice-a reduxowego jak poprzednio jednak tym razem
  * akcje będą przyjmować tzw payload jako argument dzieki temu będziemy mogli określić
  * o ile chcemy podnieść wartość naszego countera
+ *
+ * slice należy stworzyć w pliku complexCounterSlice.js (plik należy stowrzyć)
+ * slice nie będzie się bardzo różnił od tego z zadania poprzedniego
+ * jednak reducery w slicie będą teraz oprócz state przyjmowały
+ * w drugim parametrze tzw payload czyli dane przekazane w momencie dispatchowania akcji
+ * te dane (payload) posłuży nam jako wartość o którą chcemy zwiększyć lub zmniejszyć stan
+ * naszego countera
+ *
+ * Zauważ w folderze z zadaniem jest przyjęty podział na komponenty
+ * Komponent Output powinien wyświetlać dane ze slice-a (useSelector)
+ * Komponenty Increase oraz Decrease powinny dispatchować akcje
+ *
+ *
  */
 
 export const ComplexCounterWithRedux = () => {
--- src/002_basics/exercise002/TodoList.js
+++ src/002_basics/exercise002/TodoList.js
@@ -2,6 +2,27 @@ import React from "react";
 import { Form } from "./Form";
 import { List } from "./List";
 
+/**
+ * Zadanie
+ * Stwórz aplikacje typu todo list z wykorzystaniem reduxa
+ *
+ * w tym celu należy stworzyć plik przeznaczony na slice
+ * stanem slica niech będzie tablica stringów, którą
+ * będzie można updatować za pomocą formularza
+ *
+ * formularz powinien zbierać dane (znaki wpisane przez usera)
+ * za pomocą stanu lokalnego (useState), następnie
+ * na evencie submit aplikacja powinna dispatchować akcje
+ * z payloadem który pochodzi ze stanu lokalnego (useState)
+ *
+ * na tej podstawie w slice będzie można aktualizaować tablice
+ * np za pomocą metody dostępnej na tablicach .push(nowyElement)
+ *
+ * następnie wyświetl dane ze slica wyciągając je za pomocą selectora
+ * w komponencie List
+ *
+ */
+
 export const TodoList = () => {
   return (
     <article>
